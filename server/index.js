6886680696cefbfb9ff93d76e147458b7fc32df4
Trish <trishaannmonzon@gmail.com>
2025-08-20 13:57:14 +0800

    ASD

--- server/index.js
+++ server/index.js
@@ -96,12 +96,15 @@ app.use(express.json()); // For parsing JSON request bodies (for non-file upload
 // });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
+
 
 
 // Verify transporter configuration (optional, but good for debugging)
